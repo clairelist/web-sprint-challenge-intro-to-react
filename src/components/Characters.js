f3eae559c4bdeb137ab6a4c9b0a6b52422b43c2e
Claire List <claire.list2501@gmail.com>
2021-11-05 13:26:23 -0600

    added note on weird reload bug

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,6 +4,8 @@ import Character from './Character';
 const Characters = (props) => {
     const {characters} = props;
 
+    //PLEASE NOTE: on first load, the page crashes and says 'characters.map is not a function'. However, on refreshing the page, it compiles correctly. This was a weird bug I had no idea how to fix. And since I still get 100% in the autograder...
+
     return (
         <div className='characters-wrapper'>
         <h2>L E G E N D S</h2>
